c20b4029f42e6ce11c86d77069ad2b9c5bce62ec
Clemente Ferrer <80989442+clementeferrer@users.noreply.github.com>
2024-06-24 23:22:32 -0400

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Quantitative Finance Group USM",
+    page_icon="👋",
+)
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 local_css("style.css")
-
-st.set_page_config(
-    page_title="Quantitative Finance Group USM",
-    page_icon="👋",
-)
 
 st.write("# Welcome to Quantitative Finance Group USM! 👋")
 
