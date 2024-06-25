520d2616e27f013dd01273b02ea16ad807abab1a
Clemente Ferrer <80989442+clementeferrer@users.noreply.github.com>
2024-06-24 22:53:33 -0400

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Quantitative Finance Group USM",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Quantitative Finance Group USM! 👋")
+
