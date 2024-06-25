e80c39826b5455679d37933bd78d9c01b8408cc1
Clemente Ferrer <80989442+clementeferrer@users.noreply.github.com>
2024-06-24 23:14:20 -0400

    Update 3_📈_Financial time Series.py

--- pages/3_📈_Financial time Series.py
+++ pages/3_📈_Financial time Series.py
@@ -1,6 +1,11 @@
 ################################################################### Libraries
 # Streamlit
 import streamlit as st
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style.css")
 
 # Tratamiento de datos
 import pandas as pd
